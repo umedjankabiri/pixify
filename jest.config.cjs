28f76a11d87b057e1c31c030a45e56c053cda0bf
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-08 00:59:25 +0300

    Fix moduleNameMapper path for 'app' in Jest config
    Correct the moduleNameMapper path by ensuring it includes a dynamic resolution for the 'app' directory. This change allows importing modules from 'app' using the lowercase alias consistently across the project.

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
     ],
   },
   moduleNameMapper: {
-    "^App/(.*)$": "<rootDir>/src/App",
+    "^app/(.*)$": "<rootDir>/src/app/$1",
     "^common/(.*)$": "<rootDir>/src/common/$1",
     "^features/(.*)$": "<rootDir>/src/features/$1",
   },
