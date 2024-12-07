f4de70e4742b697524b800ccb768cc3c82d13654
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-08 00:08:19 +0300

    Standardize quotes and update dependencies
    Updated the code to use consistent double quotes in configuration files for improved code readability and consistency. Additionally, several dependencies were updated, including Storybook and related addons, as well as various development and testing libraries to maintain compatibility and leverage new features.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': 'src',
-            '@app': 'src/app',
-            '@components': 'src/common',
-            '@assets': 'src/assets',
-            '@features': 'src/features',
-        }
-    }
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "src",
+      "@app": "src/app",
+      "@components": "src/common",
+      "@assets": "src/assets",
+      "@features": "src/features",
+    },
+  },
+});
