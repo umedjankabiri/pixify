8b0a6d24af74da3c0d4d01dc7d6c740e749f8e08
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-08 17:35:23 +0300

    Add Modal component to App
    Introduced the Modal component to the App to display modal dialogs. This change serves as a basic setup for implementing modal functionality in the application.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
 import "app/App.css";
+import { Modal } from "common/components/Modal/Modal.tsx";
 
 function App() {
   return (
     <>
-      <h1>Start pixify project</h1>
+      <Modal open={true} />
     </>
   );
 }
