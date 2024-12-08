54d8d7f954fa66ed45d1696e3289e5e67737ceb5
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-08 17:54:37 +0300

    Use React Portal for rendering Modal component
    This change enhances the Modal component by using React Portal to render it, ensuring that modals are properly overlayed above other content. It helps in managing the component's layout more effectively, especially in complex UI structures. This update improves the overall rendering performance and UI consistency.

--- src/common/components/Modal/Modal.tsx
+++ src/common/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import stl from "common/components/Modal/Modal.module.css";
+import { createPortal } from "react-dom";
 
 type Props = {
   open: boolean;
@@ -11,16 +12,21 @@ export const Modal: FC<Props> = ({ open, modalTitle, children, onClose }) => {
   return (
     <>
       {open && (
-        <div className={stl.overlay}>
-          <div className={stl.content}>
-            <h3 className={stl.title}>{modalTitle}</h3>
-            <hr />
-            {children}
-            <button className={stl.closeIcon} onClick={onClose}>
-              x
-            </button>
-          </div>
-        </div>
+        <>
+          {createPortal(
+            <div className={stl.overlay}>
+              <div className={stl.content}>
+                <h3 className={stl.title}>{modalTitle}</h3>
+                <hr />
+                {children}
+                <button className={stl.closeIcon} onClick={onClose}>
+                  x
+                </button>
+              </div>
+            </div>,
+            document.body
+          )}
+        </>
       )}
     </>
   );
