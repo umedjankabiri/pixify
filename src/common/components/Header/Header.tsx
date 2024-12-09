fc8b90b4584140c015dd000938ea11ece05b4dd4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 22:50:41 +0300

    Add Sign Up button to Header component
    Introduce a "Sign Up" button within the Header component, enhancing user navigation to the signup page. This addition integrates seamlessly with the existing Header structure, utilizing the Button component to ensure consistent styling and behavior.

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,11 +1,15 @@
 import { Card } from "common/components/Card/Card.tsx";
 import stl from "./Header.module.css";
+import { Button } from "common/components/Button/Button.tsx";
 
 export const Header = () => {
   return (
     <div className={stl.headerWrapper}>
       <div className={stl.container}>
         <h3>logotype</h3>
+        <Button asChild>
+          <a href={"/sign-up"}>Sign Up</a>
+        </Button>
         <Card />
       </div>
     </div>
