8d43ffb4628661130da856d7f833d1dfc62b22c6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 17:03:09 +0300

    Update ModalRadix to use ModalRadixProps interface
    Replace ModalProps with ModalRadixProps in ModalRadix component for improved type specificity and clarity. This change ensures better alignment with component naming conventions and enhances maintainability.

--- src/common/components/ModalRadix/ModalRadix.tsx
+++ src/common/components/ModalRadix/ModalRadix.tsx
@@ -2,9 +2,9 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import stl from "common/components/ModalRadix/ModalRadix.module.css";
 import { FC } from "react";
-import { ModalProps } from "common/types/ModalProps/ModalProps.ts";
+import { ModalRadixProps } from "common/types/ModalRadixProps/ModalRadixProps.ts";
 
-export const ModalRadix: FC<ModalProps> = ({ open, onClose, children, title }) => (
+export const ModalRadix: FC<ModalRadixProps> = ({ open, onClose, children, title }) => (
   <Dialog.Root open={open} onOpenChange={onClose}>
     <Dialog.Portal>
       <Dialog.Overlay className={stl.Overlay} />
