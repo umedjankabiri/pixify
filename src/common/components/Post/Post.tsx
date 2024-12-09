2c5afbfc583e0b765227dce390af37bcd3bd0890
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 17:03:40 +0300

    Add Post component with delete post functionality
    Introduces a new Post component that displays a list of posts and allows users to open a modal to confirm deletion. The DeletePostModal component is utilized to handle the modal's display logic. This commit enhances the codebase by providing the ability to manage post visibility through a simple user interface.

---
+++ src/common/components/Post/Post.tsx
@@ -0,0 +1,31 @@
+import { FC, useState } from "react";
+import stl from "./Post.module.css";
+import { DeletePostModal } from "common/components/DeletePostModal/DeletePostModal.tsx";
+
+export const Post: FC = () => {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Post 1" },
+    { id: 2, title: "Post 2" },
+    { id: 3, title: "Post 3" },
+  ]);
+  const [showModal, setShowModal] = useState(false);
+
+  const openModalHandler = () => setShowModal(true);
+  const closeModalHandler = () => setShowModal(false);
+
+  const mappedPosts = posts.map((post) => {
+    return (
+      <div key={post.id} className={stl.postContainer}>
+        <h3>{post.title}</h3>
+        <button onClick={openModalHandler}>x</button>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      {mappedPosts}
+      <DeletePostModal open={showModal} onClose={closeModalHandler} />
+    </>
+  );
+};
