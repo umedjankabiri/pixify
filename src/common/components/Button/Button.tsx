8740f73e74c9b1eab3ad773175ad0b28a7d60425
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 22:06:08 +0300

    Refactor Button component to support variants.
    Replaced forwardRef with a functional component and removed ref prop. Added variant handling to apply different styles for primary, secondary, and outlined buttons. Also incorporated styles using CSS modules for cleaner code management.

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -1,6 +1,17 @@
-import { forwardRef } from "react";
-import { ButtonProps, ButtonRef } from "common/types/ButtonProps/ButtonProps.ts";
+import { FC } from "react";
+import { ButtonProps } from "common/types/ButtonProps/ButtonProps.ts";
+import stl from "./Button.module.css";
 
-export const Button = forwardRef<ButtonRef, ButtonProps>(({ ...restProps }, ref) => {
-  return <button ref={ref} {...restProps} />;
-});
+export const Button: FC<ButtonProps> = ({ variant = "primary", className, ...restProps }) => {
+  return (
+    <button
+      className={`${stl.button}
+    ${variant === "primary" ? stl.primary : ""}
+    ${variant === "secondary" ? stl.secondary : ""}
+    ${variant === "outlined" ? stl.outlined : ""}
+    ${className}
+  `}
+      {...restProps}
+    />
+  );
+};
