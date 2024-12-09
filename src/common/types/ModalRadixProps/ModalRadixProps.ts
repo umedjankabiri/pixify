b4fbbe59204a27cfe53be51588eb0632e9521964
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 22:43:10 +0300

    Refactor ModalRadixProps to include size and div props
    Updated ModalRadixProps to allow specifying a modal size with new 'ModalSize' type and included properties from div using ComponentPropsWithoutRef. This change enhances flexibility for modal customization and better component integration.

--- src/common/types/ModalRadixProps/ModalRadixProps.ts
+++ src/common/types/ModalRadixProps/ModalRadixProps.ts
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import { ComponentPropsWithoutRef } from "react";
 
+export type ModalSize = "small" | "medium" | "large";
 export type ModalRadixProps = {
   open: boolean;
   onClose: () => void;
-  children: ReactNode;
   title: string;
-};
+  size?: ModalSize;
+} & ComponentPropsWithoutRef<"div">;
