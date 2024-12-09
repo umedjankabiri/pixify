31a5e89cc05f70c3050f31c4525f56e95be83034
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 22:11:49 +0300

    Replace HTML buttons with Button component.
    Refactored the DeletePostModal to utilize the Button component for consistency and improved styling. This change enhances the overall UI alignment by leveraging the existing Button component instead of native HTML elements.

--- src/common/components/DeletePostModal/DeletePostModal.tsx
+++ src/common/components/DeletePostModal/DeletePostModal.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import { DeletePostModalProps } from "common/types/DeletePostModalProps/DeletePostModalProps.ts";
 import { ModalRadix } from "common/components/ModalRadix/ModalRadix.tsx";
+import { Button } from "common/components/Button/Button.tsx";
 
 export const DeletePostModal: FC<DeletePostModalProps> = ({ open, onClose }) => {
   return (
     <ModalRadix open={open} onClose={onClose} title={"Delete Post"}>
       Are you sure you want to delete this post?
       <div>
-        <button onClick={onClose}>Yes</button>
-        <button onClick={onClose}>No</button>
+        <Button onClick={onClose}>Yes</Button>
+        <Button onClick={onClose}>No</Button>
       </div>
     </ModalRadix>
   );
