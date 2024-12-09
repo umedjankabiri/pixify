a03a2d2beadab58854db71d0e955c460e9ecb0df
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 21:03:21 +0300

    Refactor Button component to spread props.
    This change simplifies the Button component by using the spread operator, reducing boilerplate code and improving maintainability. It ensures that all button-related properties are passed effectively, accommodating any future additions without modification.

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -1,10 +1,6 @@
 import { FC } from "react";
 import { ButtonProps } from "common/types/ButtonProps/ButtonProps.ts";
 
-export const Button: FC<ButtonProps> = ({ className, title, type, children, disabled, onClick }) => {
-  return (
-    <button className={className} type={type} disabled={disabled} title={title} onClick={onClick}>
-      {children}
-    </button>
-  );
+export const Button: FC<ButtonProps> = ({ ...restProps }) => {
+  return <button {...restProps} />;
 };
