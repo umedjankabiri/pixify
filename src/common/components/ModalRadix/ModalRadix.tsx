e6f49fb3c71dcedddcc6ed3eee0e3ad628f866c6
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 22:43:30 +0300

    Enhance ModalRadix with size and className props
    Added `size` and `className` props to the ModalRadix component, allowing more flexible styling options. Incorporated the `clsx` library to handle conditional classNames. This change enables additional customization by dynamically adjusting the modal's appearance based on specified size and additional classes.

--- src/common/components/ModalRadix/ModalRadix.tsx
+++ src/common/components/ModalRadix/ModalRadix.tsx
@@ -3,12 +3,21 @@ import { Cross2Icon } from "@radix-ui/react-icons";
 import stl from "common/components/ModalRadix/ModalRadix.module.css";
 import { FC } from "react";
 import { ModalRadixProps } from "common/types/ModalRadixProps/ModalRadixProps.ts";
+import { clsx } from "clsx";
 
-export const ModalRadix: FC<ModalRadixProps> = ({ open, onClose, children, title }) => (
-  <Dialog.Root open={open} onOpenChange={onClose}>
+export const ModalRadix: FC<ModalRadixProps> = ({
+  open,
+  onClose,
+  children,
+  title,
+  size = "medium",
+  className,
+  ...restProps
+}) => (
+  <Dialog.Root open={open} onOpenChange={onClose} {...restProps}>
     <Dialog.Portal>
       <Dialog.Overlay className={stl.Overlay} />
-      <Dialog.Content className={stl.Content}>
+      <Dialog.Content className={clsx(stl.Content, stl[size], className)}>
         <Dialog.Title className={stl.Title}>{title}</Dialog.Title>
         <hr />
         {children}
