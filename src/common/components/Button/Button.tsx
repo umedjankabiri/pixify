c4dc6264023107ee8ef6509ffe22b0b3857a707b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 22:50:33 +0300

    Add support for custom button components with asChild prop
    This update allows the Button component to render a different element by using the "asChild" prop. By leveraging Radix's Slot, developers can now pass custom components while maintaining styling and behavior.

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -2,7 +2,9 @@ import { FC } from "react";
 import { ButtonProps } from "common/types/ButtonProps/ButtonProps.ts";
 import stl from "common/components/Button/Button.module.css";
 import { clsx } from "clsx";
+import { Slot } from "@radix-ui/react-slot";
 
-export const Button: FC<ButtonProps> = ({ variant = "primary", className, ...restProps }) => {
-  return <button className={clsx(stl.button, stl[variant], className)} {...restProps} />;
+export const Button: FC<ButtonProps> = ({ variant = "primary", className, asChild, ...restProps }) => {
+  const Component = asChild ? Slot : "button";
+  return <Component className={clsx(stl.button, stl[variant], className)} {...restProps} />;
 };
