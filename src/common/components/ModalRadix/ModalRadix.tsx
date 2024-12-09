7534008c8b3456d125c9cd7f5fced2aa71a92ab4
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 16:51:35 +0300

    Refactor ModalRadix component for flexibility
    Refactor the ModalRadix component to accept props for open state management, title customization, and dynamic content rendering via children. This update enhances the component's reusability and alignment with common practices by incorporating TypeScript's FC and ModalProps for stronger type safety.

--- src/common/components/ModalRadix/ModalRadix.tsx
+++ src/common/components/ModalRadix/ModalRadix.tsx
@@ -1,36 +1,17 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import stl from "common/components/ModalRadix/ModalRadix.module.css";
+import { FC } from "react";
+import { ModalProps } from "common/types/ModalProps/ModalProps.ts";
 
-export const ModalRadix = () => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <button className={`${stl.Button} violet`}>Edit profile</button>
-    </Dialog.Trigger>
+export const ModalRadix: FC<ModalProps> = ({ open, onClose, children, title }) => (
+  <Dialog.Root open={open} onOpenChange={onClose}>
     <Dialog.Portal>
       <Dialog.Overlay className={stl.Overlay} />
       <Dialog.Content className={stl.Content}>
-        <Dialog.Title className={stl.Title}>Edit profile</Dialog.Title>
-        <Dialog.Description className={stl.Description}>
-          Make changes to your profile here. Click save when you're done.
-        </Dialog.Description>
-        <fieldset className={stl.Fieldset}>
-          <label className={stl.Label} htmlFor="name">
-            Name
-          </label>
-          <input className={stl.Input} id="name" defaultValue="Pedro Duarte" />
-        </fieldset>
-        <fieldset className={stl.Fieldset}>
-          <label className={stl.Label} htmlFor="username">
-            Username
-          </label>
-          <input className={stl.Input} id="username" defaultValue="@peduarte" />
-        </fieldset>
-        <div style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}>
-          <Dialog.Close asChild>
-            <button className={`${stl.Button} green`}>Save changes</button>
-          </Dialog.Close>
-        </div>
+        <Dialog.Title className={stl.Title}>{title}</Dialog.Title>
+        <hr />
+        {children}
         <Dialog.Close asChild>
           <button className={stl.IconButton} aria-label="Close">
             <Cross2Icon />
