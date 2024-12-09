930864f156f231ea22fd93692106ebcc36c16009
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 17:21:29 +0300

    Remove unused ModalRadix import from Header component
    This change eliminates the unused ModalRadix import to clean up the Header component. By removing this redundant code, we improve maintainability and slightly enhance performance.

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { Card } from "common/components/Card/Card.tsx";
 import stl from "./Header.module.css";
-import { ModalRadix } from "common/components/ModalRadix/ModalRadix.tsx";
 
 export const Header = () => {
   return (
@@ -8,7 +7,6 @@ export const Header = () => {
       <div className={stl.container}>
         <h3>logotype</h3>
         <Card />
-        <ModalRadix />
       </div>
     </div>
   );
