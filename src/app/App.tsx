cf45864e73cf68c807338a739a3b87c33fbd3f01
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 17:28:33 +0300

    Add Post component to App
    Include the Post component in the App for displaying individual posts. This enhances the application's functionality by allowing it to render content dynamically.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import "app/App.css";
 import { Header } from "common/components/Header/Header.tsx";
+import { Post } from "common/components/Post/Post.tsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Post />
     </>
   );
 }
