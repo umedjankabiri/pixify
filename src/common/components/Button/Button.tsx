7a4d24d7e0a6fcd9d3eae3fdc4314f38d2503db1
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-09 22:33:32 +0300

    Refactor Button component for cleaner class handling.
    Replaced string concatenation with `clsx` for managing button styles more efficiently. This change improves readability and ensures style variations are applied correctly based on the button's variant.

--- src/common/components/Button/Button.tsx
+++ src/common/components/Button/Button.tsx
@@ -1,17 +1,8 @@
 import { FC } from "react";
 import { ButtonProps } from "common/types/ButtonProps/ButtonProps.ts";
-import stl from "./Button.module.css";
+import stl from "common/components/Button/Button.module.css";
+import { clsx } from "clsx";
 
 export const Button: FC<ButtonProps> = ({ variant = "primary", className, ...restProps }) => {
-  return (
-    <button
-      className={`${stl.button}
-    ${variant === "primary" ? stl.primary : ""}
-    ${variant === "secondary" ? stl.secondary : ""}
-    ${variant === "outlined" ? stl.outlined : ""}
-    ${className}
-  `}
-      {...restProps}
-    />
-  );
+  return <button className={clsx(stl.button, stl[variant], className)} {...restProps} />;
 };
