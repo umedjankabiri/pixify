5f4dcdf9c3c2956af026680cca175c8a2d109d2b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-11 12:08:45 +0300

    Add Storybook story for ModalRadix component
    Introduce a new Storybook story for the ModalRadix component to enable interactive testing and documentation. Includes controls for props such as `open`, `onClose`, `title`, `size`, and `children`. This setup facilitates easier UI exploration and behavior validation for the modal.

---
+++ src/common/stories/ModalRadix/ModalRadix.stories.tsx
@@ -0,0 +1,47 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ModalRadix } from "common/components/ModalRadix/ModalRadix.tsx";
+import { action } from "@storybook/addon-actions";
+import { useState } from "react";
+
+const meta = {
+  title: "ModalRadix",
+  component: ModalRadix,
+  tags: ["autodocs"],
+  argTypes: {
+    open: { control: { type: "boolean" } },
+    onClose: { action: "onClose" },
+    title: { control: "text" },
+    size: {
+      control: { type: "radio" },
+      options: ["small", "medium", "large"],
+    },
+    children: { control: "text" },
+  },
+} satisfies Meta<typeof ModalRadix>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const BaseModal: Story = {
+  render: (args) => {
+    const [isOpen, setIsOpen] = useState(args.open);
+    const handleClose = () => {
+      setIsOpen(false);
+      action("onClose")();
+    };
+
+    return <ModalRadix {...args} open={isOpen} onClose={handleClose} />;
+  },
+  args: {
+    open: true,
+    onClose: action("onClose"),
+    title: "Modal title",
+    size: "medium",
+    children: (
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur assumenda consequatur cumque ea fugiat
+        illum inventore ipsam itaque non numquam, quasi quidem quisquam, quos rerum saepe voluptate? Dolor, sit?
+      </div>
+    ),
+  },
+};
