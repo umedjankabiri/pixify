84bc85439c6e4ca8838ae6482c93cedc0fdf43ca
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-11 10:17:54 +0300

    Add "LinkAsButton" story and "autodocs" tag to Button
    Included a new "LinkAsButton" story to demonstrate an outlined button with a link as its child. Added the "autodocs" tag to enable improved documentation generation for the Button component.

--- src/common/stories/Button/Button.stories.tsx
+++ src/common/stories/Button/Button.stories.tsx
@@ -5,6 +5,7 @@ import "common/components/Button/Button.module.css";
 const meta = {
   title: "Button component",
   component: Button,
+  tags: ["autodocs"],
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -52,6 +53,13 @@ export const Outlined: Story = {
   args: {
     variant: "outlined",
     children: "Outlined Button",
+    asChild: false,
+  },
+};
+export const LinkAsButton: Story = {
+  args: {
+    variant: "outlined",
+    children: <a href={"/sign-up"}>Sign Up</a>,
     asChild: false,
   },
 };
