2e62106c83e83c781ecc41311ca6ab0a8135f1b5
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-21 00:09:20 +0300

    Add CheckboxRadix component using Radix UI
    Introduced a reusable CheckboxRadix component leveraging Radix UI's checkbox and icons. This component handles controlled states and applies modular styles for consistent usage across the application.

---
+++ src/common/components/CheckboxRadix/CheckboxRadix.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { CheckIcon } from "@radix-ui/react-icons";
+import { CheckboxRadixProps } from "common/types/CheckboxRadixProps/CheckboxRadixProps.ts";
+import stl from "common/components/CheckboxRadix/Checkbox.module.css";
+import { clsx } from "clsx";
+
+export const CheckboxRadix: FC<CheckboxRadixProps> = ({ className, checked, setChecked }) => {
+  const handleChecked = (checked: boolean) => setChecked(checked);
+
+  return (
+    <div className={stl.checkContainer}>
+      <Checkbox.Root className={clsx(stl.Root, className)} checked={checked} onCheckedChange={handleChecked}>
+        <Checkbox.Indicator className={stl.Indicator}>
+          <CheckIcon />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <label className={stl.Label}>Accept terms and conditions.</label>
+    </div>
+  );
+};
