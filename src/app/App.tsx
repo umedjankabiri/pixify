762ea0c6ad495e633fbb6c7ac93049420738e714
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-21 00:09:46 +0300

    Add CheckboxRadix component to App with state management
    Integrated the CheckboxRadix component into the App component and implemented a local `checked` state using React's `useState`. This allows the checkbox's state to be dynamically managed.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import "app/App.css";
 import { Header } from "common/components/Header/Header.tsx";
 import { Post } from "common/components/Post/Post.tsx";
+import { CheckboxRadix } from "common/components/CheckboxRadix/CheckboxRadix.tsx";
+import { useState } from "react";
 
 function App() {
+  const [checked, setChecked] = useState<boolean>(false);
+
   return (
     <>
       <Header />
       <Post />
+      <CheckboxRadix checked={checked} setChecked={setChecked} />
     </>
   );
 }
