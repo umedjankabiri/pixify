998ce7e27ec02af0301035b1d054ca57bd35395f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-12-21 03:52:57 +0300

    Add SelectRadix component to App
    Integrated the SelectRadix component into the App, with two instances using predefined values. This enhances the application's UI by providing selectable dropdown functionality.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "common/components/Header/Header.tsx";
 import { Post } from "common/components/Post/Post.tsx";
 import { CheckboxRadix } from "common/components/CheckboxRadix/CheckboxRadix.tsx";
 import { useState } from "react";
+import { SelectRadix } from "common/components/SelectRadix/SelectRadix.tsx";
 
 function App() {
   const [checked, setChecked] = useState<boolean>(false);
@@ -12,6 +13,8 @@ function App() {
       <Header />
       <Post />
       <CheckboxRadix checked={checked} setChecked={setChecked} />
+      <SelectRadix value={"1"} />
+      <SelectRadix value={"2"} />
     </>
   );
 }
