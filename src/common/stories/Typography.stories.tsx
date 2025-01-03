27ecf5f75a9ae728d2a8393545576fff8f351dc4
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-01-03 13:17:10 +0300

    Add Storybook story for Typography component
    Introduce a Storybook story for the Typography component to allow visual testing and configuration of its props, including variant, color, and alignment. This facilitates easier development and demonstration of the Typography component's usage.

---
+++ src/common/stories/Typography.stories.tsx
@@ -0,0 +1,55 @@
+import { Typography } from "common/components/Typography/Typography";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Typography component",
+  component: Typography,
+  argTypes: {
+    variant: {
+      control: {
+        type: "radio",
+      },
+      options: [
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "body1",
+        "body2",
+        "subtitle1",
+        "subtitle2",
+        "caption",
+        "overline",
+        "link1",
+        "link2",
+      ],
+    },
+    color: {
+      control: {
+        type: "radio",
+      },
+      options: ["primary", "secondary", "error", "warning", "success"],
+    },
+    align: {
+      control: {
+        type: "radio",
+      },
+      options: ["inherit", "left", "center", "right", "justify", "initial", "unset"],
+    },
+  },
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DemonstrationTypographyComponent: Story = {
+  args: {
+    variant: "h1",
+    children: "Typography component",
+    asChild: false,
+  },
+};
