413c50f9d5a020d54346281eaaa5b0b965853761
umedjan kabiri <umedjan.kabiri@gmail.com>
2025-01-03 12:13:23 +0300

    Refactor ButtonProps to use a dedicated ButtonVariant type.
    This change introduces a standalone ButtonVariant type for better reusability and clarity. It streamlines type management and improves maintainability across the codebase.

--- src/common/types/ButtonProps/ButtonProps.ts
+++ src/common/types/ButtonProps/ButtonProps.ts
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef } from "react";
 
+type ButtonVariant = "primary" | "secondary" | "outlined";
 export type ButtonProps = {
-  variant?: "primary" | "secondary" | "outlined";
+  variant?: ButtonVariant;
   asChild?: boolean;
 } & ComponentPropsWithoutRef<"button">;
